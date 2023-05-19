3a7327d76798c65facd5855dee340ed6bc3ddb3b
vgav855 <132547267+vgav855@users.noreply.github.com>
2023-05-19 12:32:16 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    led.setBrightness(led.brightness() + receivedNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.setGroup(-25)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("POWER")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "POWER") {
+        encendido = !(encendido)
+        led.setBrightness(125)
+        led.enable(encendido)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(25)
+})
+let encendido = false
+radio.setGroup(13)
+encendido = false
+led.enable(encendido)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
